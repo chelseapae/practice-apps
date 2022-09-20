c54c254dc27f376a61bc877eafe5752056b61a43
Chelsea Pae <chelseapae@gmail.com>
2022-09-19 20:47:31 -0700

    Complete F1

---
+++ 2-checkout/client/src/components/F1.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const F1 = (props) => {
+  console.log('PROPS IN F1', props)
+  return (
+    <div>
+      <h1>Create Account</h1> <br/>
+      <form onSubmit={props.handleSubmitF1}>
+        <label>
+          Name: <br/>
+          <input type="text" name="name" onChange={props.onChange}/>
+        </label>
+        <br/><br/>
+
+        <label>
+          Email: <br/>
+          <input type="text" name="email" onChange={props.onChange}/>
+        </label>
+        <br/><br/>
+
+        <label>
+          Password: <br/>
+          <input type="text" name="password" onChange={props.onChange}/>
+        </label>
+        <br/><br/>
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+export default F1;
--- 2-checkout/client/src/index.jsx
+++ 2-checkout/client/src/index.jsx
@@ -7,22 +7,84 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      page: 'Homepage'
+
+      page: 'Homepage',
+
+      name: '',
+      email: '',
+      password: '',
+
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zipcode: '',
+      phone: '',
+
+      cc: '',
+      expiration: '',
+      cvv: '',
+      billingZip: ''
     }
+    this.onChange = this.onChange.bind(this);
+
+    this.handleCheckout = this.handleCheckout.bind(this);
+
+    this.handleSubmitF1 = this.handleSubmitF1.bind(this);
+  }
+
+  // General use
+  onChange(event) {
+    event.preventDefault();
+    console.log('event.target.value', event.target.value)
+    var changedValue = event.target.name
+    this.setState({
+      [changedValue]: event.target.value
+    })
+  }
+
+  // Homepage
+  handleCheckout(){
+    console.log('Checkout clicked!')
+    this.setState({
+      page: 'F1'
+    })
+  }
+
+  // F1
+  handleSubmitF1(){
+    event.preventDefault
+    this.setState({
+      page: 'F2'
+    })
   }
 
   render () {
-    return (
-      <div>
-        <p>Ready to purchase?</p>
-        <p>
-          <code>Page Cookie: {JSON.stringify(document.cookie, undefined, "\t")}</code>
-        </p>
-        <p>
-          <button type="text"> Yes take my money </button>
-        </p>
-      </div>
-    )
+    if (this.state.page === 'Homepage') {
+      return (
+        <div>
+          <p>Ready to purchase?</p>
+          <p>
+            <code>Page Cookie: {JSON.stringify(document.cookie, undefined, "\t")}</code>
+          </p>
+          <p>
+            <button type="text" onClick={this.handleCheckout}> Yes take my money </button>
+          </p>
+        </div>
+      )
+    } else if (this.state.page === 'F1') {
+      return (
+        <div>
+          <F1
+          name={this.state.name}
+          email={this.state.email}
+          password={this.state.password}
+          onChange={this.onChange}
+          handleSubmitF1={this.handleSubmitF1}
+          />
+        </div>
+      )
+    }
   }
 }
 
